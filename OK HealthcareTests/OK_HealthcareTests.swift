af5e8e4251a2511cbd0e9295a8bb1ef2f68bc4dd
Daniel Kim <danielkim@dyn-160-39-232-146.dyn.columbia.edu>
2019-03-11 18:25:05 -0400

    Committing xcode files

---
+++ OK HealthcareTests/OK_HealthcareTests.swift
@@ -0,0 +1,36 @@
+//
+//  OK_HealthcareTests.swift
+//  OK HealthcareTests
+//
+//  Created by Daniel Kim on 3/11/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import XCTest
+@testable import OK_Healthcare
+
+class OK_HealthcareTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
